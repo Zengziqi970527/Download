107b249824650e8bb501c1edca57be5e07f2f189
Zengziqi970527 <13023691319@163.com>
2023-06-06 12:31:12 +0800

    second commit

--- widget.cpp
+++ widget.cpp
@@ -1,6 +1,8 @@
 #include "widget.h"
 #include "ui_widget.h"
-
+#include<QNetworkRequest>
+#include<QDir>
+#include<QMessageBox>
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
@@ -11,5 +13,73 @@ Widget::Widget(QWidget *parent)
 Widget::~Widget()
 {
     delete ui;
+}
+
+void Widget::on_finished()
+{
+    QFileInfo fileinfo;
+    fileinfo.setFile(downloaded->fileName());
+    downloaded->close();
+    delete downloaded;
+    downloaded = Q_NULLPTR;
+    reply->deleteLater();
+    reply = Q_NULLPTR;
+    ui->btnDownLoad->setEnabled(true);
+}
+
+void Widget::on_readyread()
+{
+    downloaded->write(reply->readAll());
+}
+
+void Widget::on_downloaded(qint64 bytesRead, qint64 totalBytes)
+{
+    ui->progressBar->setMaximum(totalBytes);
+    ui->progressBar->setValue(bytesRead);
+}
+
+
+void Widget::on_btnOpenFile_clicked()
+{
+    QString curPath =QDir::currentPath();
+    QDir dir(curPath);
+    QString sub = "temp";
+    dir.mkdir(sub);
+    ui->editFilePath->setText(curPath+"/"+sub+"/");
+}
+
+
+void Widget::on_btnDownLoad_clicked()
+{
+    QString urlInput = ui->editUrl->text().trimmed();
+    if(urlInput.isEmpty()){
+        QMessageBox::information(this,"错误","请输入下载文件地址");
+        return;
+    }
+    QUrl url = QUrl::fromUserInput(urlInput);
+    if(!url.isValid()){
+        QMessageBox::information(this,"错误",QString("无效url:\n%1\n错误信息:\n%2\n").arg(urlInput,url.errorString()));
+        return;
+    }
+    QString tempDir = ui->editFilePath->text().trimmed();
+    if(tempDir.isEmpty()){
+        QMessageBox::information(this,"错误","Please Input Save Path");
+        return;
+    }
+    QString fileName = tempDir+url.fileName();
+    if(QFile::exists(fileName))
+        QFile::remove(fileName);
+    downloaded = new QFile(fileName);
+    if(!downloaded->open(QIODevice::WriteOnly)){
+        QMessageBox::information(this,"错误","临时文件打开错误");
+        return;
+    }
+    ui->btnDownLoad->setEnabled(false);
+    reply = networkmanager.get(QNetworkRequest(url));
+    connect(reply,&QNetworkReply::finished,this,&Widget::on_finished);
+    connect(reply,&QNetworkReply::readyRead,this,&Widget::on_readyread);
+    void(QNetworkReply::*download)(qint64,qint64) = &QNetworkReply::downloadProgress;
+    void(Widget::*on_download)(qint64,qint64) = &Widget::on_downloaded;
+    connect(reply,download,this,on_download);
 }
 
--- widget.h
+++ widget.h
@@ -2,7 +2,9 @@
 #define WIDGET_H
 
 #include <QWidget>
-
+#include<QNetworkAccessManager>
+#include<QNetworkReply>
+#include<QFile>
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
 QT_END_NAMESPACE
@@ -10,10 +12,20 @@ QT_END_NAMESPACE
 class Widget : public QWidget
 {
     Q_OBJECT
-
+private:
+    QNetworkAccessManager networkmanager;
+    QNetworkReply *reply;
+    QFile *downloaded;
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
+private slots:
+    void on_finished();
+    void on_readyread();
+    void on_downloaded(qint64 bytesRead,qint64 totalBytes);
+    void on_btnOpenFile_clicked();
+
+    void on_btnDownLoad_clicked();
 
 private:
     Ui::Widget *ui;
